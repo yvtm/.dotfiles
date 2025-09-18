91b9a61c1a4017e605c857ce49a9e46b6a9ab00d
yvette <yvtm@posteo.net>
2025-09-18 00:06:48 -0700

    fixed wallpaper.sh

--- .config/hypr/wallpaper.sh
+++ .config/hypr/wallpaper.sh
@@ -7,7 +7,13 @@ menu() {
 main() {
     choice=$(menu | wofi -c ~/.config/wofi/wallpaper -s ~/.config/wofi/style-wallpaper.css --show dmenu --prompt "Select Wallpaper:" -n)
     selected_wallpaper=$(echo "$choice" | sed 's/^img://')
-    swww img "$selected_wallpaper" --transition-type any --transition-fps 60 --transition-duration .5
+    swww img "$selected_wallpaper" --outputs HDMI-A-1 --transition-type any --transition-fps 60 --transition-duration .5
+    if [[ "$selected_wallpaper" == *.* ]]; then
+      new="${selected_wallpaper%.*}1.${selected_wallpaper##*.}"
+    else
+      new="${selected_wallpaper}1"
+    fi
+    swww img "${new}" --outputs DP-3 --transition-type any --transition-fps 60 --transition-duration .5
     wal -i "$selected_wallpaper" -n --cols16
     swaync-client --reload-css
     cat ~/.cache/wal/colors-kitty.conf > ~/.config/kitty/current-theme.conf
